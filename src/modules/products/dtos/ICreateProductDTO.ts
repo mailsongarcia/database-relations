be8809fe768476aa10bffc1566c59c30d182c7a5
mailsongarcia <mailsongarcialima@gmail.com>
2020-07-14 10:15:27 -0400

    FInish

--- src/modules/products/dtos/ICreateProductDTO.ts
+++ src/modules/products/dtos/ICreateProductDTO.ts
@@ -1,3 +1,5 @@
+import Customer from '@modules/customers/infra/typeorm/entities/Customer';
+
 export default interface ICreateProductDTO {
   name: string;
   price: number;
--- src/modules/products/dtos/IUpdateProductsQuantityDTO.ts
+++ src/modules/products/dtos/IUpdateProductsQuantityDTO.ts
@@ -1,3 +1,5 @@
+import { StringifyOptions } from 'querystring';
+
 export default interface IUpdateProductsQuantityDTO {
   id: string;
   quantity: number;
